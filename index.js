a29d83d08b374316642a84c0e1462f409334f81b
ammarsharif <ammarsharif377@gmail.com>
2024-06-27 15:56:41 +0500

    plan added in index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const connectDB = require('./config/db');
 
 const profileRoutes = require('./routes/profile');
 const subscriptionRoutes = require('./routes/subscription');
+const subscriptionPlanRoutes = require('./routes/subscriptionPlan');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ connectDB();
 
 app.use('/api/profile', profileRoutes);
 app.use('/api/subscription', subscriptionRoutes);
+app.use('/api/subscriptionPlan', subscriptionPlanRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
